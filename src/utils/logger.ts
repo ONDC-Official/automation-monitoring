17249e94fbf8d2ac272ea95aa6573c108a0f95dc
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2024-11-20 18:10:23 +0530

    added grafana

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,7 @@
 import winston from "winston";
 import "winston-daily-rotate-file";
 import { config } from "../config/serverConfig";
+import LokiTransport from "winston-loki";
 
 const { combine, timestamp, printf, colorize, errors } = winston.format;
 
@@ -23,13 +24,21 @@ const logger = winston.createLogger({
       format: combine(colorize(), logFormat),
     }),
 
-    // Daily file rotation for production logs
-    new winston.transports.DailyRotateFile({
-      dirname: "logs",
-      filename: "application-%DATE%.log",
-      datePattern: "YYYY-MM-DD",
-      maxFiles: "30d", // Keep logs for 30 days
-      zippedArchive: true,
+    // // Daily file rotation for production logs
+    // new winston.transports.DailyRotateFile({
+    //   dirname: "logs",
+    //   filename: "application-%DATE%.log",
+    //   datePattern: "YYYY-MM-DD",
+    //   maxFiles: "30d", // Keep logs for 30 days
+    //   zippedArchive: true,
+    // }),
+    new LokiTransport({
+      host: "http://localhost:3100",
+      labels: { app: "automation-tool" },
+      json: true,
+      format: winston.format.json(),
+      replaceTimestamp: true,
+      onConnectionError: (err: any) => logger.error(err),
     }),
   ],
 });
